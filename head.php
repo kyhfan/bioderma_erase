e6e492bde28ab41284ae99e3431ad76536652b7d
kyhfan <kyhfan@gmail.com>
2018-03-02 11:43:50 +0900

    ga 코드 삽입

--- head.php
+++ head.php
@@ -22,4 +22,13 @@
 		<script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
 
 		<script src="./js/common.js"></script>
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114911663-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'UA-114911663-1');
+		</script>		
 	</head>
--- m/head.php
+++ m/head.php
@@ -26,4 +26,14 @@
 		<script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
 		
 		<script src="./js/common.js"></script>
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114911663-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'UA-114911663-1');
+		</script>		
+		
 	</head>
