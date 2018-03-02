9ab0f6464376550f4e10fca03c3d68471ffe0071
kyhfan <kyhfan@gmail.com>
2018-03-02 18:26:18 +0900

    22222

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
             <div class="c-header c-header--main">
 				<div class="c-header__aligner">
-					<h1 class="c-h1"><a href="index.php" class="c-h1__logo"><img src="./images/common/c-logo.png" alt="BIODERMA" /></a></h1>
+					<h1 class="c-h1"><a href="http://www.bioderma.co.kr/" target="_blank" class="c-h1__logo"><img src="./images/common/c-logo.png" alt="BIODERMA" /></a></h1>
 					<div class="c-sns">
 						<button class="c-sns__button c-sns__button--facebook" onclick="wmbt.sns_share('fb');">페이스북</button>
 						<button class="c-sns__button c-sns__button--kakao" onclick="wmbt.sns_share('ks');">카카오스토리</button>
 					</div>
 					<div class="c-events">
-						<button class="c-event c-event--1"><span>EVENT 1</span>건강한 피부만 남기다</button>
+						<button class="c-event c-event--1" onclick="location.href='index.php'"><span>EVENT 1</span>건강한 피부만 남기다</button>
 						<button class="c-event c-event--2" onclick="alert('4월에 오픈됩니다')"><span>EVENT 2</span>투고 키트 샘플링</button>
 					</div>
 				</div>
--- m/header.php
+++ m/header.php
@@ -1,6 +1,6 @@
 			<div class="c-header">
 				<div class="c-header__aligner">
-					<h1 class="c-h1"><a href="index.php" class="c-h1__logo"><img src="./images/common/c-logo.png" alt="BIODERMA" /></a></h1>
+					<h1 class="c-h1"><a href="http://www.bioderma.co.kr/" target="_blank" class="c-h1__logo"><img src="./images/common/c-logo.png" alt="BIODERMA" /></a></h1>
 					<a href="#" class="c-drawer__trigger">
 						<span class="c-drawer__burger c-drawer__burger--top">메뉴</span>
 						<span class="c-drawer__burger c-drawer__burger--middle">여</span>
@@ -10,8 +10,8 @@
 				<div class="c-drawer">
 					<div class="c-drawer__aligner">
 						<div class="c-events">
-							<button class="c-event c-event--1"><span>EVENT 1</span>건강한 피부만 남기다</button>
-							<button class="c-event c-event--2"><span>EVENT 2</span>투고 키트 샘플링</button>
+							<button class="c-event c-event--1" onclick="location.href='index.php';"><span>EVENT 1</span>건강한 피부만 남기다</button>
+							<button class="c-event c-event--2" onclick="alert('4월에 오픈됩니다')"><span>EVENT 2</span>투고 키트 샘플링</button>
 						</div>
 						<div class="c-sns">
 							<button class="c-sns__button c-sns__button--facebook" onclick="sns_share('fb')">페이스북</button>
