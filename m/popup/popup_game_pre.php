66bc23f7d5d204ef3d5623318815a9623925f5b9
ojw <ojoonwoo@gmail.com>
2018-03-05 12:48:13 +0900

    게임 시작전 팝업 추가

---
+++ m/popup/popup_game_pre.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <!-- <meta n ame="viewport" content="width=device-width, initial-scale=1.0" /> -->
+    <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>BIODERMA</title>
+    <meta name="format-detection" content="telephone=no" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="메이크업은 말끔하게 지우고, 건강한 피부만 남겨요!">
+    <meta property="og:url" content="http://www.biodermaevent.com" />
+    <meta property="og:image" content="http://www.biodermaevent.com/images/share_image.jpg" />
+    <meta property="og:description" content="제한시간 20초! 3개의 화려한 메이크업 말끔하게 지우면 바이오더마 정품 팡팡!">
+
+    <link rel="stylesheet" href="../css/common.css" />
+    <link rel="stylesheet" href="../css/page.css" />
+    <link type="image/icon" rel="shortcut icon" href="http://www.biodermaevent.com/images/favicon.ico" />
+
+    <script src="../lib/jquery/jquery-1.9.1.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="../lib/placeholder/jquery.placeholder.min.js"></script>
+    <script src="../lib/gsap/TweenMax.min.js"></script>
+    <script src="../lib/gsap/jquery.gsap.min.js"></script>
+    <script src="../lib/eraser/jquery.eraser.js"></script>
+    <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script>
+    <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+
+    <script src="../js/common.js"></script>
+
+</head>
+<body>
+    <div class="c-popup game-pre c-popup--active" onclick="popupClose(this)">
+        <div class="c-popup__aligner">
+           <div class="c-popup__box">
+               <div class="c-popup__heading"><h3>게임 참여방법</h3></div>
+               <div class="c-popup__image"><img src="../images/common/c-popup_game_pre_image.png" alt="" /></div>
+                <div class="c-popup__buttons">
+                    <button type="button" class="c-popup__button">
+                        <img src="../images/common/c-popup__pre-button.png" alt="GAME START">
+                    </button>
+                </div>
+           </div>
+        </div>
+    </div>
+    </body>
+</html>
