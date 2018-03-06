62885eee772ff1249d7082928d77dad76fe08ed9
kyhfan <kyhfan@gmail.com>
2018-03-06 16:07:29 +0900

    222

--- include/mnv_function.php
+++ include/mnv_function.php
@@ -106,7 +106,7 @@ class mnv_function extends mnv_dbi
 		$kit_winner_count       = 7000;	// 투고 키트 총 당첨 수량
 		$goods_winner_count     = 6;	// 정품 총 당첨 수량
 
-        $kit_array      = array("Y","N","N","N","N");
+        $kit_array      = array("Y","N","N","N","N","N","N","N","N","N");
         // $kit_array      = array("Y","N");
 		shuffle($kit_array);
 		
