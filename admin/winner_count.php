af3faae79411db07e837d1a69dfa083a7529f55f
kyhfan <kyhfan@gmail.com>
2018-03-08 12:16:10 +0900

    데일리 당첨 갯수

--- admin/winner_count.php
+++ admin/winner_count.php
@@ -1,11 +1,9 @@
 <?php
-
 	// 설정파일
 	include_once "../include/autoload.php";
 
 	$mnv_f = new mnv_function();
 	$my_db         = $mnv_f->Connect_MySQL();
-
 /*
 	if (isset($_SESSION['ss_mb_id']) == false)
 	{
@@ -14,90 +12,70 @@
 		exit; 
 	}
 */
-
 	include "./head.php";
-
-	if(isset($_REQUEST['pg']) == false)
-		$pg = "1";
-	else
-		$pg = $_REQUEST['pg'];
-	
-	if (!$pg)
-		$pg = "1";
-	//if(isset($pg) == false) $pg = 1;	// $pg가 없으면 1로 생성
-	$page_size = 10;	// 한 페이지에 나타날 개수
-	$block_size = 10;	// 한 화면에 나타낼 페이지 번호 개수
-
-	//if (isset($search_type) == false)
-	//	$search_type = "search_by_name";
 ?>
+
+
 <div id="page-wrapper">
   <div class="container-fluid">
   <!-- Page Heading -->
     <div class="row">
       <div class="col-lg-12">
-        <h1 class="page-header">경품별 당첨자 수</h1>
+        <h1 class="page-header">일자별 경품 당첨자 수</h1>
       </div>
     </div>
-    <!-- /.row -->
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="table-responsive">
-          <table id="entry_list" class="table table-hover">
-            <thead>
-              <tr>
-                <th>정품(6명)</th>
-                <th>투고키트(7000명)</th>
-                <th>꽝</th>
-              </tr>
-            </thead>
-            <tbody>
-<?php 
-	$buyer_count_query = "SELECT count(*) FROM member_info WHERE 1";
-
-	$buyer_list_query = "SELECT mb_winner, count(mb_winner) cnt FROM member_info WHERE 1 GROUP BY mb_winner";
-	$res = mysqli_query($my_db, $buyer_list_query);
-
-	while ($b_data = @mysqli_fetch_array($res))
+      <!-- /.row -->
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="table-responsive">
+            <div id="daily_topgirl_vote_count_div1" style="display:block">
+              <table class="table table-hover">
+                <thead>
+                  <tr><th>날짜</th><th>정품(6명)</th><th>투고키트(7000명)</th><th>꽝</th></tr>
+                </thead>
+                <tbody>
+<?php
+	$daily_date_query	= "SELECT mb_regdate FROM member_info Group by substr(mb_regdate,1,10) order by mb_regdate desc";
+	$date_res			= mysqli_query($my_db, $daily_date_query);
+	while($date_daily_data = mysqli_fetch_array($date_res))
 	{
-		if ($b_data['mb_winner'] == "goods")
+		$daily_date		= substr($date_daily_data['mb_regdate'],0,10);
+		$winner_query	= "SELECT mb_winner, COUNT( mb_winner ) winner_cnt FROM member_info WHERE mb_regdate LIKE  '%".$daily_date."%' GROUP BY mb_winner";
+		$winner_res		= mysqli_query($my_db, $winner_query);
+		
+		unset($winner_cnt);
+		$total_winner_cnt = 0;
+		while ($winner_daily_data = mysqli_fetch_array($winner_res))
 		{
-			$winner_goods	= $b_data['cnt'];
-		}else if ($b_data['mb_winner'] == "kit"){
-			$winner_kit	= $b_data['cnt'];
-		}else{
-			$winner_blank	= $b_data['cnt'];
+			if ($winner_daily_data['mb_winner'] == "goods")
+			{
+				$winner_goods	= $winner_daily_data['winner_cnt'];
+			}else if ($winner_daily_data['mb_winner'] == "kit"){
+				$winner_kit	= $winner_daily_data['winner_cnt'];
+			}else{
+				$winner_blank	= $winner_daily_data['winner_cnt'];
+			}
+	
+?>
+                  <tr>
+                    <td><?=$daily_date?></td>
+                    <td><?=number_format($winner_goods)?></td>
+                    <td><?=number_format($winner_kit)?></td>
+                    <td><?=number_format($winner_blank)?></td>
+                  </tr>
+<?
 		}
 	}
 ?>
-              <tr>
-                <td><?php echo $winner_goods?></td>	<!-- No. 하나씩 감소 -->
-                <td><?php echo $winner_kit?></td>	<!-- No. 하나씩 감소 -->
-                <td><?php echo $winner_blank?></td>	<!-- No. 하나씩 감소 -->
-              </tr>
-            </tbody>
-          </table>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
+        <!-- /.row -->
       </div>
-      <!-- /.row -->
+      <!-- /.container-fluid -->
     </div>
-    <!-- /.container-fluid -->
+    <!-- /#page-wrapper -->
   </div>
-  <!-- /#page-wrapper -->
-</div>
-<!-- /#wrapper -->
-</body>
-
-</html>
-
-
-
-<script type="text/javascript">
- 
-	function pageRun(num)
-	{
-		f = document.frm_execute;
-		f.pg.value = num;
-		f.submit();
-	}
-</script>
+  <!-- /#wrapper -->
