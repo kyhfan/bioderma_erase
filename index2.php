5c189051325b58efff03c6b7193a5453b277af36
kyhfan <kyhfan@gmail.com>
2018-03-23 09:58:19 +0900

    22222

--- index2.php
+++ index2.php
@@ -10,7 +10,35 @@
     </head>
     <body>
         <div class="wrap">
+            <div class="main-header">
+                <div class="header-wrap">
+                    <div class="header-sns-area">
+                        <img src="./images/event2/share_facebook_btn.png" alt="페이스북 공유 버튼">
+                        <img src="./images/event2/share_kakaostory_btn.png" alt="카카오스토리 공유 버튼">
+                    </div>
+                    <div class="header-navi">
+                        <div>
+                            <span>EVENT 1</span>
+                            <span>건강한 피부만 남기다</span>
+                        </div>
+                        <div>
+                            <span>EVENT 2</span>
+                            <span>투-고 키트 샘플링</span>
+                        </div>
+                    </div>
+                    <div class="header-logo">
+                        <img src="./images/event2/logo.png" alt="로고">
+                    </div>
+                </div>
+            </div>
             <div class="main-section1">
+                <div class="main-title">
+                    <span>투고키트 후기이벤트</span>
+                    <br />
+                    <span>지난 이벤트에서 투고키트를 받지 못하신 분들에게 추첨을 통해 투고키트를 드립니다</span>
+                    <br />
+                    <span>지난 참여를 통해 당첨된 분은 당첨 선발에서 제외됩니다 ( EVENT1. 클렌징 타임 포함)</span>
+                </div>
             </div>
         </div>
         <script>
