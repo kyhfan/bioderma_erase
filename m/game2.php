a677eca19200a471f88d92bcb1ab3c28c5c15699
kyhfan <kyhfan@gmail.com>
2018-04-09 10:17:33 +0900

    33333

--- m/game2.php
+++ m/game2.php
@@ -78,7 +78,7 @@ include_once "./head2.php";
 						<li>사업자등록번호: 214-88-79685 <a href="http://www.ftc.go.kr/bizCommPop.do?wrkr_no=&apv_perm_no=2015321015330200215" target="_blank">(사업자정보확인)</a></li>
 						<li>주소: 서울특별시 서초구 서초중앙로 138 우림빌딩 7층 <br>나오스코리아 유한회사</li>
 						<li>개인정보책임자: 김민정</li>
-						<li>이벤트 안내번호: 070-4888-1164</li>
+						<li>이벤트 안내번호: 02-523-7676</li>
 						<li>통신판매업신고번호: 2015-서울서초-0215</li>
 						<li>E-MAIL: bioderma@bioderma.kr</li>
 					</ul>
