0776999f3ac03a4ed07ae738f492e56ad0a468ae
kyhfan <kyhfan@gmail.com>
2018-04-16 11:31:43 +0900

    5555

--- index.php
+++ index.php
@@ -99,20 +99,23 @@
                             <!-- Slides -->
                             <div class="swiper-slide">
                                 <div class="image_area">
-                                    <img src="./images/event2/sample_best_image.jpg" alt="">
+                                    <!-- <img src="./images/event2/sample_best_image.jpg" alt=""> -->
+                                    <img src="./images/event2/week1_best_image.jpg" alt="" width="561">
                                 </div>
                                 <div class="content_area">
                                     <div class="profile_area">
-                                        <img src="./images/event2/sample_best_profileimage.png" alt="">
-                                        <span>Biodermaevent</span>
+                                        <!-- <img src="./images/event2/sample_best_profileimage.png" alt=""> -->
+                                        <img src="./images/event2/week1_best_profile.jpg" alt="" width="69">
+                                        <span>yell_cc_</span>
                                     </div>
                                     <div class="desc_area">
-                                        메이크업은 말끔하게 지우고<br>
-                                        건강한 피부만 남기는 바이오더마!<br>
-                                        이제 밖에서도 간편하게<br>
-                                        투고키트로 즐길 수 있어서 최고<br><br>
-                                        #바이오더마 #클렌징워터<br>
-                                        #투고키트 #클렌징지존
+                                        가방에 쏙!<br>
+                                        완젹한 클렌징, 요만한거 없는듯💗<br><br>
+                                        #바이오더마#센시비오#클렌징워터#한정판#투고키트#클렌징지존<br>
+                                        #좋아요반사#20대#다이어트#인친#오픈카톡#다이렉트#좋아요#<br>
+                                        l4l#소통#일상#디톡스#27살#훈녀#모닝스타그램#나스타그램<br>
+                                        #협찬#일요일#쥬얼리#목걸이#주말#대학생#직장인#뷰스타그램<br>
+                                        #수원#용인#대구
                                     </div>
                                     <div class="color_area">
                                     </div>
